ef10e6b9970678727bb185fd15622b6e35a3ea44
ajmal-j <ajmaljaleel7860@gmail.com>
2023-11-13 00:45:03 +0530

    some style changes in the header and footer

--- public/jsFiles/alert.js
+++ public/jsFiles/alert.js
@@ -4,62 +4,68 @@ function showAlert(message) {
     alertDiv.style.position = 'fixed';
     alertDiv.style.right = '20px';
     alertDiv.style.zIndex = '2000000000000000';
-    alertDiv.style.top = '60px';
+    alertDiv.style.top = '-100px'; 
     alertDiv.style.opacity = '0';
-    alertDiv.style.transition = 'opacity 0.5s ease-in-out';
-    alertDiv.style.backgroundColor = '#D80032'; // Red
-    alertDiv.style.color = 'white';
+    alertDiv.style.transition = 'opacity 0.5s ease-in-out, top 0.5s ease-in-out';
+    alertDiv.style.backgroundColor = '#ffff';
+    alertDiv.style.color = 'black';
     alertDiv.style.padding = '15px';
     alertDiv.style.marginBottom = '15px';
-    alertDiv.style.borderRadius = '4px';
+    alertDiv.style.borderRadius = '15px';
     alertDiv.style.boxShadow = '0 2px 15px 0 rgba(0,0,0,0.24), 0 5px 5px 0 rgba(0,0,0,0.19)';
     alertDiv.setAttribute('role', 'alert');
-    alertDiv.innerHTML = `<strong>${message}</strong>`;
+    alertDiv.innerHTML = `<div style="display: flex; align-items: center;">
+      <i class="fa-solid fa-circle-xmark fs-4 border-circle my-auto fa-shake" style="color:#da2c38;"></i>
+      <span class="my-auto">&nbsp;<strong>${message}</strong></span>
+    </div>`;
     document.body.appendChild(alertDiv);
-
-    // Fade in
+    alertDiv.offsetHeight;
+    alertDiv.style.top = '60px';
     setTimeout(() => {
-        alertDiv.style.opacity = '1';
+      alertDiv.style.opacity = '1';
     }, 50);
-
-    // Fade out and remove
     setTimeout(() => {
-        alertDiv.style.opacity = '0';
-        setTimeout(() => {
-            alertDiv.remove();
-        }, 500);
+      alertDiv.style.opacity = '0';
+      alertDiv.style.top = '-100px'; 
+      setTimeout(() => {
+        alertDiv.remove();
+      }, 500);
     }, 2000);
-}
+  }
+  
 
 function showSuccess(message) {
     const alertDiv = document.createElement('div');
     alertDiv.className = 'alert';
     alertDiv.style.position = 'fixed';
     alertDiv.style.right = '20px';
-    alertDiv.style.zIndex = '200000000000000000000000';
-    alertDiv.style.top = '60px';
+    alertDiv.style.zIndex = '2000000000000000';
+    alertDiv.style.top = '-100px'; 
     alertDiv.style.opacity = '0';
-    alertDiv.style.transition = 'opacity 0.5s ease-in-out';
-    alertDiv.style.backgroundColor = '#00A36C'; // Green
-    alertDiv.style.color = 'white';
+    alertDiv.style.transition = 'opacity 0.5s ease-in-out, top 0.5s ease-in-out';
+    alertDiv.style.backgroundColor = '#ffff';
+    alertDiv.style.color = 'black';
     alertDiv.style.padding = '15px';
     alertDiv.style.marginBottom = '15px';
-    alertDiv.style.borderRadius = '4px';
+    alertDiv.style.borderRadius = '15px';
     alertDiv.style.boxShadow = '0 2px 15px 0 rgba(0,0,0,0.24), 0 5px 5px 0 rgba(0,0,0,0.19)';
     alertDiv.setAttribute('role', 'alert');
-    alertDiv.innerHTML = `<strong>${message}</strong>`;
+    alertDiv.innerHTML = `<div style="display: flex; align-items: center;">
+      <i class="fa-solid fa-circle-check fs-4 border-circle my-auto fa-shake" style="color:#16db65;"></i>
+      <span class="my-auto">&nbsp;<strong>${message}</strong></span>
+    </div>`;
     document.body.appendChild(alertDiv);
-
-     // Fade in
-     setTimeout(() => {
-        alertDiv.style.opacity = '1';
-     }, 50);
-
-     // Fade out and remove
-     setTimeout(() => {
-        alertDiv.style.opacity = '0';
-        setTimeout(() => {
-            alertDiv.remove();
-        }, 500);
-     }, 2000);
-}
+    alertDiv.offsetHeight;
+    alertDiv.style.top = '60px';
+    setTimeout(() => {
+      alertDiv.style.opacity = '1';
+    }, 50);
+    setTimeout(() => {
+      alertDiv.style.opacity = '0';
+      alertDiv.style.top = '-100px'; 
+      setTimeout(() => {
+        alertDiv.remove();
+      }, 500);
+    }, 2000);
+  }
+  
--- public/jsFiles/homePage.js
+++ public/jsFiles/homePage.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const productImageMain2 = document.querySelectorAll('.productImageMain2');
-    const firstImage = document.querySelector('.firstImage');
     let isMouseOver = false;
 
     productImageMain2.forEach((element) => {
@@ -14,14 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   
     setInterval(() => {
-        var currentOpacity = window.getComputedStyle(firstImage).getPropertyValue('opacity');
-        if (currentOpacity === '1') {
-            firstImage.style.opacity = '0.9999';
-            firstImage.style.transform = 'scale(1.08)';
-        } else {
-            firstImage.style.opacity = '1';
-            firstImage.style.transform = 'scale(1)';
-        }
         if (!isMouseOver) {
             productImageMain2.forEach((element) => {
                 element.style.opacity = (element.style.opacity === '1') ? '0' : '1';
